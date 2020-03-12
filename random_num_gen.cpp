ea771eebff8f5478b870b3ab7dbfb91ae6459360
ShaunakM <shaunakm@users.noreply.github.com>
2020-03-12 19:50:20 -0400

    Tested with random shuffle

--- random_num_gen.cpp
+++ random_num_gen.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <ctime>
+#include <random>
 
 using namespace std;
 
@@ -11,6 +12,9 @@ int main()
     srand((unsigned)time(0));
     vector<int> choices(2*TOTAL_TEAMS);
     int choices_no = 1;
+
+    random_device random_dev;
+    mt19937 generate(random_dev());
 
     for(int i=0; i<(2*TOTAL_TEAMS); i++)
     {
@@ -26,9 +30,21 @@ int main()
     }
     cout<<endl;
 
-    for(int i=0; i<TOTAL_TEAMS; i++)
+    // for(int i=0; i<TOTAL_TEAMS; i++)
+    // {
+    //     int random_index = rand() % choices.size();
+    //     cout<<"Random number is :"<<choices[random_index]<<endl;
+    // }
+
+    for(int i=0; i<5; i++)
     {
-        int random_index = rand() % choices.size();
-        cout<<"Random number is :"<<choices[random_index]<<endl;
+        shuffle(choices.begin(), choices.end(), generate);
+        cout<<"Choices"<<endl;
+        // for(int i=0; i<(2*TOTAL_TEAMS); i++)
+        for(int i=0; i<choices.size(); i++)
+        {
+            cout<<choices[i]<<",";
+        }
+        cout<<endl;
     }
 }
