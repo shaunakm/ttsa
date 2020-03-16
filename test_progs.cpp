7199bae47450512691f97139243461105f6a46b1
ShaunakM <shaunakm@users.noreply.github.com>
2020-03-16 16:28:38 -0400

    test prog to check different cpp functions

---
+++ test_progs.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a;
+    unsigned int b;
+    cout<<"enter 2 numbers: ";
+    cin>>a>>b;
+
+    cout<<"\nNumbers are "<<a<<" and "<<b<<endl;
+    cout<<"Size of numbers are "<<sizeof(a)<< " and "<<sizeof(b)<<endl;
+
+    return 0;
+}
